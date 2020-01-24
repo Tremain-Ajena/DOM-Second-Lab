f4f8805600dab3a973718ffa41436a2376ddc965
Tremain-Ajena <tremainajena.j@gmail.com>
2020-01-24 01:11:08 -0500

    first steps

---
+++ scripts.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let button = document.createElement('button');
+    let btnText = document.createTextNode('Add Square');
+    button.appendChild(btnText);
+    document.body.appendChild(button);
+
+
+    button.addEventListener('click', function () {
+        btnClicked();
+    })
+
+    function btnClicked () {
+        let sqHolder = document.createElement('div');
+        sqHolder.className = 'squareHolder';
+        let inText = document.createElement('div');
+        sqHolder.appendChild(inText);
+        document.body.appendChild(sqHolder);
+    }
+});
